390f97986bc5153038b3f1359b49a0474a60976d
Lykke27 <usachev97@gmail.com>
2021-03-30 21:59:26 +0300

    fix bug with users presentation

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,16 +5,52 @@ import styles from "./Users.module.css"
 type UsersPropsType = MapDispatchPropsType & MapStatePropsType;
 
 const Users = (props: UsersPropsType) => {
+    if (props.users.length===0) {
+        props.setUsers([
+            {
+                id: 1,
+                photoUrl: "https://i.pinimg.com/474x/a0/1e/b9/a01eb920157d569f0c214bc48ef1dec4.jpg",
+                followed: false, fullName: "Kirill", status: 'Hello!', location: {city: "Moscow", country: "Russia"}
+            },
+            {
+                id: 2,
+                photoUrl: "https://i.pinimg.com/474x/a0/1e/b9/a01eb920157d569f0c214bc48ef1dec4.jpg",
+                followed: true,
+                fullName: "Andrew",
+                status: "It's my first status",
+                location: {city: "SPB", country: "Russia"}
+            },
+            {
+                id: 3,
+                photoUrl: "https://i.pinimg.com/474x/a0/1e/b9/a01eb920157d569f0c214bc48ef1dec4.jpg",
+                followed: false, fullName: "Sasha", status: "What's up?", location: {city: "Kiev", country: "Ukraine"}
+            },
+            {
+                id: 4,
+                photoUrl: "https://i.pinimg.com/474x/a0/1e/b9/a01eb920157d569f0c214bc48ef1dec4.jpg",
+                followed: false, fullName: "Sveta", status: "Hello there", location: {city: "Minsk", country: "Belarus"}
+            },
+            {
+                id: 5,
+                photoUrl: "https://i.pinimg.com/474x/a0/1e/b9/a01eb920157d569f0c214bc48ef1dec4.jpg",
+                followed: true,
+                fullName: "Alex",
+                status: "Here is the status",
+                location: {city: "Moscow", country: "Russia"}
+            },
+        ])
+    }
+
     return (
         <div>
-            {
-                props.users.map(user => <div key={user.id}>
+            {props.users.map(user =>
+                    <div key={Math.random()}>
                         <span>
-                            <div><img className={styles.userAvatar} src={user.photoUrl} alt=""/></div>
+                            <div><img className={styles.userAvatar} src={user.photoUrl} alt="profile picture"/></div>
                             <div>
                                 {user.followed
-                                    ? <button onClick={()=>{props.unfollow(user.id)}}>Unfollow</button>
-                                    : <button onClick={()=>{props.follow(user.id)}}>Follow</button>}
+                                    ? <button onClick={() => {props.unfollow(user.id)}}>Unfollow</button>
+                                    : <button onClick={() => {props.follow(user.id)}}>Follow</button>}
                             </div>
                         </span>
                         <span>
@@ -26,11 +62,12 @@ const Users = (props: UsersPropsType) => {
                                 <div>{user.location.country}</div>
                                 <div>{user.location.city}</div>
                             </span>
-
                         </span>
-                    </div>)
+                    </div>
+                    )
             }
         </div>
+
     )
 }
 
--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -1,28 +1,11 @@
-import {ActionsTypes} from "./redux-store";
+import {ActionsTypes, FollowActionType, SetUsersActionType, UnfollowActionType} from "./redux-store";
 
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = "SET_USERS";
 
 let initialState = {
-    users: [
-        {id: 1,
-            photoUrl: "https://i.pinimg.com/474x/a0/1e/b9/a01eb920157d569f0c214bc48ef1dec4.jpg",
-            followed: false, fullName: "Kirill", status: 'Hello!', location: {city: "Moscow", country: "Russia"}},
-        {id: 2,
-            photoUrl: "https://i.pinimg.com/474x/a0/1e/b9/a01eb920157d569f0c214bc48ef1dec4.jpg",
-            followed: true, fullName: "Andrew", status: "It's my first status", location: {city: "SPB", country: "Russia"}},
-        {id: 3,
-            photoUrl: "https://i.pinimg.com/474x/a0/1e/b9/a01eb920157d569f0c214bc48ef1dec4.jpg",
-            followed: false, fullName: "Sasha", status: "What's up?", location: {city: "Kiev", country: "Ukraine"}},
-        {id: 4,
-            photoUrl: "https://i.pinimg.com/474x/a0/1e/b9/a01eb920157d569f0c214bc48ef1dec4.jpg",
-            followed: false, fullName: "Sveta", status: "Hello there", location: {city: "Minsk", country: "Belarus"}},
-        {id: 5,
-            photoUrl: "https://i.pinimg.com/474x/a0/1e/b9/a01eb920157d569f0c214bc48ef1dec4.jpg",
-            followed: true, fullName: "Alex", status: "Here is the status", location: {city: "Moscow", country: "Russia"}},
-    ] as Array<UserType>,
-    newPostText: ""
+    users: [] as Array<UserType>
 };
 
 const usersReducer = (state: InitialStateType = initialState, action: ActionsTypes): InitialStateType => {
@@ -50,7 +33,9 @@ const usersReducer = (state: InitialStateType = initialState, action: ActionsTyp
             }
 
         case SET_USERS: {
-            return {...state, users:[...state.users, ...action.users]}
+            return {
+                ...state,
+                users:[...action.users]}
         }
 
         default:
@@ -64,6 +49,7 @@ type UsersPageType = {
     users: Array<UserType>
 }
 // type UserType = typeof Users
+
 export type UserType = {
     id: number
     photoUrl: string
@@ -77,9 +63,8 @@ export type UserType = {
         }
 }
 
-export const followAC = (userID:number) => ({type: FOLLOW, userID})
-export const unfollowAC = (userID:number) => ({type: UNFOLLOW, userID})
-export const setUsersAC = (users:Array<UserType>) => ({type: SET_USERS, users})
-
+export const followAC = (userID:number):FollowActionType => ({type: FOLLOW, userID})
+export const unfollowAC = (userID:number):UnfollowActionType => ({type: UNFOLLOW, userID})
+export const setUsersAC = (users:Array<UserType>):SetUsersActionType => ({type: SET_USERS, users})
 
 export default usersReducer;
