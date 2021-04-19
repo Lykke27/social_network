28283437f57d970bf0cdbb1bc35821ddb5eb4d7e
Lykke27 <usachev97@gmail.com>
2021-04-19 20:10:36 +0300

    add disclaimer to news tab

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -4,7 +4,9 @@ import style from './News.module.css'
 const News = () => {
     return (
         <div className={style.newsPage}>
-            News
+            Для разработки приложения используется API с сайта <a href="https://social-network.samuraijs.com/">https://social-network.samuraijs.com/</a>.
+            Данные с API доступны только для зарегистрированных пользователей, поэтому на данный момент на GitHub-pages вкладки Profile и Find Users в бесконечной загрузке.
+            Скоро добавится функция регистрации и все будет в порядке, stay tuned!
         </div>
     )
 }
